d2cdbe12f5c6081532d4be2f6892f75bbceedaa8
Bai Xue <bai@er-10-132-141-87.wireless.umn.edu>
2018-03-18 11:03:53 -0500

    Add Ornstein-Uhlenbeck Noise

---
+++ ounoise.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+class OUNoise:
+    def __init__(self, size, mu, theta, sigma):
+        self.mu = mu * np.ones(size)
+        self.theta = theta
+        self.sigma = sigma
+        self.reset()
+
+    def reset(self):
+        self.state = self.mu
+
+    def sample(self):
+        x = self.state
+        dx = self.theta * (self.mu - x) + self.sigma * np.random.randn(len(x))
+        self.state = x + dx
+        return self.state
+    
